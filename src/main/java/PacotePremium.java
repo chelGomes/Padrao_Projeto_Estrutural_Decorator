fecf8c8c80b60626e4f433617f45a3983eb286c8
chelGomes <68570496+chelGomes@users.noreply.github.com>
2023-10-03 02:24:45 -0300

    modelagem

--- src/main/java/PacotePremium.java
+++
@@ -1,9 +0,0 @@
-public class PacotePremium implements PacoteTv{
-    public String getDescricao(){
-        return "Pacote Premium ";
-    }
-
-    public double getCusto(){
-        return 80.0;
-    }
-}
